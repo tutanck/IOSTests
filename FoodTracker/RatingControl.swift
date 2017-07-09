d766b5575d69fc6456eb29156220a5b6169e93a5
Tutanck <tutanck@gmail.com>
2017-07-09 20:44:54 +0200

    button pressed

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -22,6 +22,14 @@ class RatingControl: UIStackView {
         setupButtons()
     }
     
+    //MARK: Button Action
+    
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
+
+    
     //MARK: Private Methods
     
     private func setupButtons() {
@@ -35,6 +43,9 @@ class RatingControl: UIStackView {
         button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
         button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
         
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+         
         // Add the button to the stack
         addArrangedSubview(button)
         
