38e18326b2f71c5cc85d760320867458c3b09039
Tutanck <tutanck@gmail.com>
2017-07-09 15:33:15 +0200

    tap gesture recognizer added to controllerView

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -46,6 +46,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Actions
 
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+    }
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         
         mealNameLabel.text = "Default Text"
