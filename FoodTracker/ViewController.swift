cd906eb7be3b864482fda949c295203ae4213579
Tutanck <tutanck@gmail.com>
2017-07-09 20:24:10 +0200

    redButton checkpoint

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -44,6 +44,29 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     
+    //MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+        
+    }
+
+    
     //MARK: Actions
 
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
